958bb9ea2931338303f37e317db48bedd9e90aeb
otskelly <151046115+otskelly@users.noreply.github.com>
2024-01-10 22:39:48 +0200

    Add files via upload

--- cypress/e2e/issue-comments.cy.js
+++ cypress/e2e/issue-comments.cy.js
@@ -1,3 +1,7 @@
+import IssueComments from "../pages/IssueComments";
+
+
+
 describe('Issue comments creating, editing and deleting', () => {
     beforeEach(() => {
         cy.visit('/');
@@ -68,4 +72,80 @@ describe('Issue comments creating, editing and deleting', () => {
             .find('[data-testid="issue-comment"]')
             .should('not.exist');
     });
+
+    //Test combination: Comments Functionality
+    it('Should create a new comment successfully', () => {
+        const commentAsk = 'Be nice as Halo';
+        getIssueDetailsModal().within(() => {
+            cy.contains('Add a comment...')
+                .click();
+
+            cy.get('textarea[placeholder="Add a comment..."]').type(commentAsk);
+
+            cy.contains('button', 'Save')
+                .click()
+                .should('not.exist');
+
+            cy.contains('Add a comment...').should('exist');
+            cy.get('[data-testid="issue-comment"]').should('contain', commentAsk);
+        });
+    });
+
+    it('Previous comment edited successfully', () => {
+        const NewComment= 'Making Halo write';
+        const previousComment = 'An old silent pond...';
+        
+
+        getIssueDetailsModal().within(() => {
+            cy.get('[data-testid="issue-comment"]')
+                .first()
+                .contains('Edit')
+                .click()
+                .should('not.exist');
+
+            cy.get('textarea[placeholder="Add a comment..."]')
+                .should('contain', previousComment)
+                .clear()
+                .type(NewComment);
+
+            cy.contains('button', 'Save')
+                .click()
+                .should('not.exist');
+
+            cy.get('[data-testid="issue-comment"]')
+                .should('contain', 'Edit')
+                .and('contain', NewComment);
+        });
+    });
+
+    it('Should delete a new edited comment successfully', () => {
+        getIssueDetailsModal()
+            .find('[data-testid="issue-comment"]')
+            .contains('Delete')
+            .click();
+
+        cy.get('[data-testid="modal:confirm"]')
+            .contains('button', 'Delete comment')
+            .click()
+            .should('not.exist');
+
+        getIssueDetailsModal()
+            .find('[data-testid="issue-comment"]')
+            .should('not.exist');
+    });
+
+
+
+//Addcomments
+it('Using POM Adding Comment, editing comment and deleting comment', () => {
+    IssueComments.AddComment();
+    IssueComments.AssertThatCommentIsVisible();
+    IssueComments.EditComment();
+    IssueComments.AssertThatEditedCommetIsVisible();
+    //IssueComments.DeleteComment(); -> not working
+   // IssueComments.AssertThatCommentIsDeleted();
 });
+
+});
+
+
